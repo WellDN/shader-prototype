7261f976a8ee00ca2ebafb4f96d164f3bd226548
w <w@pop-os.localdomain>
2022-10-25 17:49:15 -0300

    :octocat:

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useRef } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
 import { extend } from '@react-three/fiber'
-import { shaderMaterial } from '@react-three/drei'
+import { MeshDistortMaterial, MeshWobbleMaterial, shaderMaterial } from '@react-three/drei'
 import glsl from 'babel-plugin-glsl/macro'
 
 
-const WaveMaterial = shaderMaterial(
+const FormlessMaterial = shaderMaterial(
   {
-    time: 0,
     colorOne: new THREE.Color(),
     colorTwo: new THREE.Color(),
   },
@@ -22,16 +21,16 @@ const WaveMaterial = shaderMaterial(
         vUv = uv;
       }`,
   glsl`
-  #pragma glslify: snoise4 = require(glsl-noise/classic/4d.glsl)
+  #pragma glslify: pnoise4 = require(glsl-noise/classic/4d.glsl);
   uniform float time;
   uniform vec3 colorOne;
   uniform vec3 colorTwo;
   varying vec2 vUv;
   void main() {
-    vec2 displacedUv = vUv + snoise4(vec4(vUv * 9.0, -5.0, time * 0.05));
-    float strength = snoise4(vec4(displacedUv - 10.0, 0.0, time * 0.2));
-    float outerGlow = distance(vUv, vec2(0.5)) * 2.0 - 0.5;
-    strength += outerGlow;
+    vec2 displacedUv = vUv + pnoise4(vec4(vUv * 9.0, 1.0, time * 0.05));
+    float strength = pnoise4(vec4(displacedUv * 1.0, -1.0, time * 0.2));
+    float outerGlow = distance(vUv, vec2(0.5)) * 1.0 + 0.5;
+    strength += outerGlow * strength;
     strength += step(-0.2, strength) * 0.6;
     strength = clamp(strength, 0.0, 1.0);
     vec3 color = mix(colorOne, colorTwo, strength);
@@ -40,18 +39,18 @@ const WaveMaterial = shaderMaterial(
         #include <encodings_fragment>
       }`
 )
-
-extend({ WaveMaterial })
-
+        //THE OBJECT IS STATIC, JUST THE COLOR THAT CHANGES
+extend({ FormlessMaterial })
 
 function ShaderPlane() {
   const ref = useRef()
   const { width, height } = useThree((state) => state.viewport)
-  useFrame((state, delta) => (ref.current.time += delta))
+  useFrame((state, delta) => (ref.current.color += delta))
   return (
     <mesh scale={[width, height, 1]}>
-      <planeGeometry />
-      <waveMaterial ref={ref} key={WaveMaterial.key} toneMapped={true} colorOne={'#AA2929'} colorTwo={'#000000'} />
+      <planeBufferGeometry />
+      <MeshDistortMaterial distort={1} speed={5} />
+      <formlessMaterial ref={ref} key={FormlessMaterial.key} toneMapped={true} colorOne={'#AA2929'} colorTwo={'#000000'} />
     </mesh>
   )
 }
