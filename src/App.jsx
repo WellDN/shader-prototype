acf778adc82b6d538d456abd9acaee4e03778d8d
w <w@pop-os.localdomain>
2022-10-26 17:28:10 -0300

    :octocat:

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,35 @@ import { useRef } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
 import { extend } from '@react-three/fiber'
-import { MeshDistortMaterial, MeshWobbleMaterial, shaderMaterial } from '@react-three/drei'
+import { MeshDistortMaterial, shaderMaterial } from '@react-three/drei'
 import glsl from 'babel-plugin-glsl/macro'
 
 
 const FormlessMaterial = shaderMaterial(
   {
+    tie: 0,
     colorOne: new THREE.Color(),
     colorTwo: new THREE.Color(),
   },
   glsl`
-      varying vec2 vUv;
-      void main() {
-        vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-        vec4 viewPosition = viewMatrix * modelPosition;
-        vec4 projectionPosition = projectionMatrix * viewPosition;
-        gl_Position = projectionPosition;
-        vUv = uv;
-      }`,
+  varying vec2 vUv;
+  void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+    gl_Position = projectionPosition;
+    vUv = uv;
+  }`,
   glsl`
   #pragma glslify: pnoise4 = require(glsl-noise/classic/4d.glsl);
-  uniform float time;
+  uniform float tie;
   uniform vec3 colorOne;
   uniform vec3 colorTwo;
   varying vec2 vUv;
   void main() {
-    vec2 displacedUv = vUv + pnoise4(vec4(vUv * 9.0, 1.0, time * 0.05));
-    float strength = pnoise4(vec4(displacedUv * 1.0, -1.0, time * 0.2));
-    float outerGlow = distance(vUv, vec2(0.5)) * 1.0 + 0.5;
+    vec2 displacedUv = vUv + pnoise4(vec4(vUv * 9.0, 0, tie * 0.05));
+    float strength = pnoise4(vec4(displacedUv * 1.0, 0, tie * 0.2));
+    float outerGlow = distance(vUv, vec2(0.5)) * 2.0 - 0.5;
     strength += outerGlow * strength;
     strength += step(-0.2, strength) * 0.6;
     strength = clamp(strength, 0.0, 1.0);
@@ -38,17 +39,18 @@ const FormlessMaterial = shaderMaterial(
         #include <tonemapping_fragment>
         #include <encodings_fragment>
       }`
-)
-        //THE OBJECT IS STATIC, JUST THE COLOR THAT CHANGES
-extend({ FormlessMaterial })
-
-function ShaderPlane() {
-  const ref = useRef()
-  const { width, height } = useThree((state) => state.viewport)
-  useFrame((state, delta) => (ref.current.color += delta))
-  return (
+      )
+      //THE OBJECT IS STATIC, JUST THE COLOR THAT CHANGES
+      extend({ FormlessMaterial })
+      
+      
+      function ShaderPlane() {
+        const ref = useRef()
+        const { width, height } = useThree((state) => state.viewport)
+        useFrame((state, delta) => (ref.current.tie += delta))
+        return (
     <mesh scale={[width, height, 1]}>
-      <planeBufferGeometry />
+      <torusKnotBufferGeometry args={[2, 3, 3, 6]} />
       <MeshDistortMaterial distort={1} speed={5} />
       <formlessMaterial ref={ref} key={FormlessMaterial.key} toneMapped={true} colorOne={'#AA2929'} colorTwo={'#000000'} />
     </mesh>
