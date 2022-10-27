6c6201c56537ffec51074fcd089b4de4ef6d3a3f
WellDN <Wellingtontp0@gmail.com>
2022-10-27 15:35:00 -0300

    :octocat:

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,34 @@ import glsl from 'babel-plugin-glsl/macro'
 
 const FormlessMaterial = shaderMaterial(
   {
-    tie: 0,
+    time: 0,
     colorOne: new THREE.Color(),
     colorTwo: new THREE.Color(),
   },
   glsl`
-  varying vec2 vUv;
-  void main() {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    vec4 viewPosition = viewMatrix * modelPosition;
-    vec4 projectionPosition = projectionMatrix * viewPosition;
-    gl_Position = projectionPosition;
-    vUv = uv;
-  }`,
+      varying vec2 vUv;
+      void main() {
+        vec4 modelPosition = modelMatrix * vec4(position, 2.0);
+        vec4 viewPosition = viewMatrix * modelPosition;
+        vec4 projectionPosition = projectionMatrix * viewPosition;
+        gl_Position = projectionPosition;
+        vUv = uv;
+      }`,
   glsl`
-  #pragma glslify: pnoise4 = require(glsl-noise/classic/4d.glsl);
-  uniform float tie;
-  uniform vec3 colorOne;
-  uniform vec3 colorTwo;
-  varying vec2 vUv;
-  void main() {
-    vec2 displacedUv = vUv + pnoise4(vec4(vUv * 9.0, 0, tie * 0.05));
-    float strength = pnoise4(vec4(displacedUv * 1.0, 0, tie * 0.2));
-    float outerGlow = distance(vUv, vec2(0.5)) * 2.0 - 0.5;
-    strength += outerGlow * strength;
-    strength += step(-0.2, strength) * 0.6;
-    strength = clamp(strength, 0.0, 1.0);
-    vec3 color = mix(colorOne, colorTwo, strength);
-    gl_FragColor = vec4(color, 1.0);
+      #pragma glslify: cnoise3 = require(glsl-noise/classic/3d.glsl) 
+      uniform float time;
+      uniform vec3 colorOne;
+      uniform vec3 colorTwo;
+      varying vec2 vUv;
+      void main() {
+        vec2 displacedUv = vUv + cnoise3(vec3(vUv * 1.0, time * 0.05));
+        float strength = cnoise3(vec3(displacedUv * 10.0, time * 0.2));
+        float outerGlow = distance(vUv, vec2(0.5)) * 2.0 - 0.5;
+        strength += outerGlow;
+        strength += step(-0.2, strength) * 0.6;
+        strength = clamp(strength, 0.0, 1.0);
+        vec3 color = mix(colorOne, colorTwo, strength);
+        gl_FragColor = vec4(color, 1.0);
         #include <tonemapping_fragment>
         #include <encodings_fragment>
       }`
@@ -47,10 +47,10 @@ const FormlessMaterial = shaderMaterial(
       function ShaderPlane() {
         const ref = useRef()
         const { width, height } = useThree((state) => state.viewport)
-        useFrame((state, delta) => (ref.current.tie += delta))
+        useFrame((state, delta) => (ref.current.time += delta))
         return (
     <mesh scale={[width, height, 1]}>
-      <torusKnotBufferGeometry args={[2, 3, 3, 6]} />
+      <torusKnotBufferGeometry args={[1, 1, 19, 90, 20, 5]} />
       <MeshDistortMaterial distort={1} speed={5} />
       <formlessMaterial ref={ref} key={FormlessMaterial.key} toneMapped={true} colorOne={'#AA2929'} colorTwo={'#000000'} />
     </mesh>
