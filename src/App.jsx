f3ed7b05f681a301cba5a0f73c1413d9e00e43fd
WellDN <Wellingtontp0@gmail.com>
2022-10-29 20:28:58 -0300

    :octocat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,74 @@
-import { useRef } from 'react'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
-import { extend } from '@react-three/fiber'
-import { MeshDistortMaterial, shaderMaterial } from '@react-three/drei'
-import glsl from 'babel-plugin-glsl/macro'
+import React, { Suspense, useRef, useMemo } from 'react'
+import { Canvas, useThree, useFrame } from '@react-three/fiber'
+import { useGLTF, useFBO, Stage, Effects } from '@react-three/drei'
+import { FXAAShader } from 'three-stdlib'
+import { AdditiveBlendingShader } from './shaders/AdditiveBlendingShader'
+import { VolumetricLightShader } from './shaders/VolumetricLightShader'
 
+const DEFAULT_LAYER = 0
+const OCCLUSION_LAYER = 1
 
-const FormlessMaterial = shaderMaterial(
-  {
-    time: 0,
-    colorOne: new THREE.Color(),
-    colorTwo: new THREE.Color(),
-  },
-  glsl`
-      varying vec2 vUv;
-      void main() {
-        vec4 modelPosition = modelMatrix * vec4(position, 2.0);
-        vec4 viewPosition = viewMatrix * modelPosition;
-        vec4 projectionPosition = projectionMatrix * viewPosition;
-        gl_Position = projectionPosition;
-        vUv = uv;
-      }`,
-  glsl`
-      #pragma glslify: cnoise4 = require(glsl-noise/simplex/3d.glsl);
-      uniform float time;
-      uniform vec3 colorOne;
-      uniform vec3 colorTwo;
-      varying vec2 vUv;
-      void main() {
-        vec2 displacedUv = vUv + cnoise4(vec3(vUv * 1.0, time * 0.02));
-        float strength = cnoise4(vec3(displacedUv * 10.0, time * 0.2));
-        
-        float outerGlow = distance(vUv, vec2(0.4)) * 0.1 * 0.9;
+function Elf({ layer = DEFAULT_LAYER }) {
+  const group = useRef()
+  const { nodes } = useGLTF('/scene.glb')
+  const material = useMemo(() => {
+    if (layer === DEFAULT_LAYER) return new THREE.MeshStandardMaterial({ color: new THREE.Color('#2a2a2a'), roughness: 1, metalness: 0.9 })
+    else return new THREE.MeshBasicMaterial({ color: new THREE.Color('black') })
+  }, [layer])
 
-        strength += outerGlow;
-        strength += step(-0.2, strength) * 0.6;
-        strength = clamp(strength, 0.0, 1.0);
-        vec3 color = mix(colorOne, colorTwo, strength);
-        gl_FragColor = vec4(color, 1.0);
-        #include <tonemapping_fragment>
-        #include <encodings_fragment>
-      }`
-    )
-      //THE OBJECT IS STATIC, JUST THE COLOR THAT CHANGES
-      extend({ FormlessMaterial })
-      
-      
-      function ShaderPlane() {
-        const ref = useRef()
-        const { width, height } = useThree((state) => state.viewport)
-        useFrame((state, delta) => (ref.current.time += delta))
-        return (
-    <mesh scale={[width, height, 1]}>
-      <torusKnotBufferGeometry args={[1, 0.9, 19, 30, 20, 5]} />
-      <MeshDistortMaterial distort={1} speed={5} />
-      <formlessMaterial ref={ref} key={FormlessMaterial.key} toneMapped={true} colorOne={'#AA2929'} colorTwo={'#000000'} />
-    </mesh>
+  useFrame(() => (group.current.rotation.y += 0.004))
+
+  return (
+    <group ref={group} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]} position={[0, 2, 0]}>
+        <mesh geometry={nodes.mesh_0.geometry} material={material} layers={layer} receiveShadow castShadow />
+        <mesh geometry={nodes.mesh_1.geometry} material={material} layers={layer} receiveShadow castShadow />
+        <mesh geometry={nodes.mesh_2.geometry} material={material} layers={layer} receiveShadow castShadow />
+      </group>
+    </group>
+  )
+}
+
+function Post() {
+  const { gl, camera, size } = useThree()
+  const occlusionRenderTarget = useFBO()
+  const occlusionComposer = useRef()
+  const composer = useRef()
+  useFrame(() => {
+    camera.layers.set(OCCLUSION_LAYER)
+    occlusionComposer.current.render()
+    camera.layers.set(DEFAULT_LAYER)
+    composer.current.render()
+  }, 1)
+  return (
+    <>
+      <mesh layers={OCCLUSION_LAYER} position={[1, 12, -10]}>
+        <sphereGeometry args={[4, 32, 32]} />
+        <meshBasicMaterial />
+      </mesh>
+      <Effects ref={occlusionComposer} disableGamma disableRender args={[gl, occlusionRenderTarget]} renderToScreen={false}>
+        <shaderPass args={[VolumetricLightShader]} needsSwap={false} />
+      </Effects>
+      <Effects ref={composer} disableRender>
+        <shaderPass args={[AdditiveBlendingShader]} uniforms-tAdd-value={occlusionRenderTarget.texture} />
+        <shaderPass args={[FXAAShader]} uniforms-resolution-value={[1 / size.width, 1 / size.height]} renderToScreen />
+      </Effects>
+    </>
   )
 }
 
 export default function App() {
   return (
-    <Canvas>
-      <ShaderPlane />
+    <Canvas camera={{ position: [0, 0, 12], fov: 35 }} gl={{ antialias: false }}>
+      <color attach="background" args={['#050505']} />
+      <Suspense fallback={null}>
+        <Stage intensity={2}>
+          <Elf />
+          <Elf layer={OCCLUSION_LAYER} />
+        </Stage>
+        <Post />
+      </Suspense>
     </Canvas>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import './style.css'
+import './styles.css'
 import App from './App'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.querySelector('#root')).render(<App />)
+//make it tsx.
